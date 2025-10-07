4435250b0dccb96fe4baaddde656e6c0238fb905
chumachenkoUA <kirito22122005@gmail.com>
2025-10-07 17:19:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
-'use strict';
+'use stdict';
 
-// write your code here
+const table = document.querySelector('table');
+const tHeadRows = [...table.tHead.rows];
+const tBodyRows = [...table.tBodies[0].rows];
+const tFootRows = [...table.tFoot.rows];
+
+const newColumn = (tEntity) => {
+  tEntity.forEach((row) => {
+    const newCell = row.cells[1].cloneNode(true);
+
+    row.insertBefore(newCell, row.lastElementChild);
+  });
+};
+
+newColumn(tHeadRows);
+newColumn(tBodyRows);
+newColumn(tFootRows);
